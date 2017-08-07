fe06e4c170cdfbb722a01bda9353ac58e3fa839c
Denis Pavlovic <denis.pav@hotmail.com>
2017-08-07 23:33:33 +0200

    Moved everything to MVC

---
+++ MagicLinks/MagicLinks/Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using MagicLinks.Database;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MagicLinks.Controllers
+{
+    [Route("~/")]
+    public class HomeController : Controller
+    {
+        private IDataProtector Protector { get; set; }
+        private IMemoryCache Cache { get; set; }
+        private DatabaseContext Db { get; set; }
+
+        public HomeController(DatabaseContext db, IDataProtectionProvider protectionProvider, IMemoryCache cache)
+        {
+            this.Db = db;
+            this.Protector = protectionProvider.CreateProtector("magic");
+            this.Cache = cache;
+        }
+
+        [HttpGet()]
+        public IActionResult Index()
+            => Ok(new
+            {
+                Magic = $"{Program.URL}magic/email@gmail.com"
+            });
+
+        [HttpGet("magic/{email}")]
+        public async Task<IActionResult> MagicLink(string email)
+        {
+            if (await Db.Users.AnyAsync(x => x.Email == email))
+            {
+                var data = Protector.Protect(email);
+                var magic = $"{Program.URL}login/{data}";
+
+                Cache.Remove(email);
+                Cache.GetOrCreate(email, entry => data);
+
+                return Ok(new
+                {
+                    MagicLink = magic
+                });
+            }
+            else
+            {
+                return NotFound(new
+                {
+                    Message = "Email not found"
+                });
+            }
+        }
+
+        [HttpGet("login/{token}")]
+        public async Task<IActionResult> MagicLogin(string token)
+        {
+            var email = Protector.Unprotect(token);
+
+            if (Cache.Get(email) != null)
+            {
+                var user = await Db.Users.FirstOrDefaultAsync(x => x.Email == email);
+
+                var claims = new List<Claim> {
+                         new Claim(ClaimTypes.Name, user.Id.ToString()),
+                         new Claim(ClaimTypes.NameIdentifier, user.Email)
+                };
+
+                var identity = new ClaimsIdentity(claims, "cookies");
+                var principal = new ClaimsPrincipal(identity);
+
+                await HttpContext.Authentication.SignInAsync("AuthScheme", principal);
+
+                return Ok(new
+                {
+                    Message = "Loged in successfully"
+                });
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new
+                {
+                    Message = "Something went wrong"
+                });
+            }
+        }
+
+        [HttpGet(nameof(Protected))]
+        [Authorize(Policy = "testPolicy")]
+        public IActionResult Protected()
+         => Ok(new
+         {
+             Message = "Route with heavy protection"
+         });
+    }
+}
--- MagicLinks/MagicLinks/Startup.cs
+++ MagicLinks/MagicLinks/Startup.cs
@@ -2,14 +2,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.DataProtection;
-using Microsoft.Extensions.Caching.Memory;
-using System.Security.Claims;
-using System.Collections.Generic;
 using System;
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Threading.Tasks;
 
 namespace MagicLinks
 {
@@ -19,9 +14,11 @@ namespace MagicLinks
         {
             services.AddDataProtection();
             services.AddDbContext<DatabaseContext>(opts => opts.UseInMemoryDatabase());
-            services.AddRouting();
             services.AddAuthorization(opts => opts.AddPolicy("testPolicy", builder => builder.RequireAuthenticatedUser()));
             services.AddMemoryCache();
+            services.AddMvcCore()
+                .AddJsonFormatters()
+                .AddAuthorization();
         }
 
         public void Configure(IApplicationBuilder app, DatabaseContext context)
@@ -33,75 +30,14 @@ namespace MagicLinks
                 AutomaticAuthenticate = true,
                 AutomaticChallenge = true,
                 ExpireTimeSpan = TimeSpan.FromDays(10),
+                Events = new CookieAuthenticationEvents
+                {
+                    OnRedirectToLogin = ctx => Task.CompletedTask
+                }
             });
 
             app.UseDeveloperExceptionPage();
-
-            var router = new RouteBuilder(app);
-
-            router.MapGet("magic/{email}", async (HttpContext ctx) =>
-            {
-                var email = ctx.GetRouteValue("email").ToString();
-                var db = ctx.RequestServices.GetRequiredService<DatabaseContext>();
-                var protector = ctx.RequestServices.GetRequiredService<IDataProtectionProvider>().CreateProtector("magic");
-                var cache = ctx.RequestServices.GetRequiredService<IMemoryCache>();
-                var user = await db.Users.FirstOrDefaultAsync(x => x.Email == email);
-
-                if (user != null)
-                {
-                    var data = protector.Protect(user.Email);
-                    var magic = $"{Program.URL}login/{data}";
-
-                    cache.Remove(user.Email);
-                    cache.GetOrCreate(user.Email, entry => data);
-
-                    await ctx.Response.WriteAsync(magic);
-                }
-                else
-                {
-                    await ctx.Response.WriteAsync("");
-                }
-            });
-
-            router.MapGet("login/{token}", async (HttpContext ctx) =>
-            {
-                var protector = ctx.RequestServices.GetRequiredService<IDataProtectionProvider>().CreateProtector("magic");
-                var db = ctx.RequestServices.GetRequiredService<DatabaseContext>();
-                var email = protector.Unprotect(ctx.GetRouteValue("token").ToString());
-                var cache = ctx.RequestServices.GetRequiredService<IMemoryCache>();
-
-                if (cache.Get(email) != null)
-                {
-                    var user = await db.Users.FirstOrDefaultAsync(x => x.Email == email);
-
-                    var claims = new List<Claim> {
-                         new Claim(ClaimTypes.Name, Guid.NewGuid().ToString()),
-                         new Claim(ClaimTypes.NameIdentifier, user.Email)
-                    };
-                    var identity = new ClaimsIdentity(claims, "cookies");
-                    var principal = new ClaimsPrincipal(identity);
-
-                    await ctx.Authentication.SignInAsync("AuthScheme", principal);
-                    await ctx.Response.WriteAsync("Done signing in");
-                }
-
-                await ctx.Response.WriteAsync("");
-            });
-
-            router.MapGet("protected", async (HttpContext ctx) =>
-            {
-                var authService = ctx.RequestServices.GetRequiredService<IAuthorizationService>();
-                if (await authService.AuthorizeAsync(ctx.User, "testPolicy"))
-                {
-                    await ctx.Response.WriteAsync("Protected resource");
-                }
-                else
-                {
-                    await ctx.Response.WriteAsync("Please login in order to view this");
-                }
-            });
-
-            app.UseRouter(router.Build());
+            app.UseMvc();
         }
     }
 }
