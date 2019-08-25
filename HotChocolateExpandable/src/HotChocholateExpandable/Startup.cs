f847c9aa7887d52ae9597ef79fa2e76fdb17a699
Denis Pavlović <denis.pav@hotmail.com>
2019-08-25 11:06:48 +0200

    Added initial data for models

--- HotChocolateExpandable/src/HotChocholateExpandable/Startup.cs
+++ HotChocolateExpandable/src/HotChocholateExpandable/Startup.cs
@@ -1,17 +1,28 @@
-﻿using HotChocolate;
+﻿using HotChocholateExpandable.Database;
+using HotChocolate;
 using HotChocolate.AspNetCore;
 using HotChocolate.AspNetCore.Voyager;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace HotChocholateExpandable
 {
     public class Startup
     {
+        readonly IConfiguration Config;
+
+        public Startup(IConfiguration config)
+        {
+            Config = config;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<DatabaseContext>(opts => opts.UseSqlite(Config["Db"]).EnableSensitiveDataLogging(true));
             services.AddGraphQL(sp => Schema.Create(opts =>
             {
                 opts.RegisterServiceProvider(sp);
